62f3d2423494804d8c4320dd9eac8318ae6e160e
Jack Gitter <jack.gitter@gmail.com>
2024-06-29 15:16:42 -0400

    basic jwt stuff working!

--- server/initializer.go
+++ server/initializer.go
@@ -3,11 +3,24 @@ package server
 import (
 	//"bytes"
 	//"encoding/json"
+	//	"fmt"
+	"bytes"
 	"fmt"
+	"time"
+
+	//"io"
+	//"time"
+
+	//"encoding/json"
+	//"fmt"
 	"net/http"
+
+	//"net/url"
+
 	//"net/url"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func InitializeHttpServer() *gin.Engine {
@@ -25,40 +38,57 @@ func helloWorld(c *gin.Context) {
 }
 
 func auth(c *gin.Context) {
-    //scope := "user-read-private user-read-email"
-    //redirectUri := "http://localhost:80"
-    endpoint := "https://accounts.spotify.com/authorize?response_type=code&client_id=83ada5f0555a4f57be4243c3788cc9f4&scope=user-read-private&redirect_uri=http://localhost:2000/accesstoken"
 
+    endpoint := "https://accounts.spotify.com/authorize?response_type=code&client_id=83ada5f0555a4f57be4243c3788cc9f4&scope=user-read-private&redirect_uri=http://localhost:2000/accesstoken"
     c.Redirect(http.StatusMovedPermanently, endpoint) 
 
 }
 
 func accessToken(c *gin.Context) {
-    fmt.Println(c.Query("code"))
 
-   /* body := url.Values{}
-    body.Set("grant_type", "authorization_code")
-    body.Set("code", c.Query("code"))
-    body.Set("redirect_uri", "http://localhost:2000/accesstoken")
+    b := "grant_type=authorization_code&code=" + c.Query("code") + "&redirect_uri=http://localhost:2000/accesstoken"
 
-    jsonString, _ := json.Marshal(body)
-    req, _ := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBuffer(jsonString))
+    req, _ := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBuffer([]byte(b)))
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     req.Header.Set("Authorization", "Basic ODNhZGE1ZjA1NTVhNGY1N2JlNDI0M2MzNzg4Y2M5ZjQ6ODVkYjM4OTFiYzU0NDNmZGIxZDM2MDRjZjM5YTBhM2I=" )
 
     client := &http.Client{}
-    resp, _ := client.Do(req) 
-    fmt.Println(resp)*/
-    c.Redirect(http.StatusFound, "http://localhost:8080")
+    client.Do(req) 
+
+    claims :=  jwt.MapClaims{
+		"sub": "test",
+		"iss": "todo-app",                 
+		"aud": "role",
+		"exp": time.Now().Add(time.Hour).Unix(), 
+		"iat": time.Now().Unix(),
+        "spotifyID": "testID",
+        "accessToken": "testToken",
+        "refreshToken": "testRefresh",
+        "accessTokenExpiresAt": "testDate",
+    }        
 
 
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
+    tokenString, err := token.SignedString([]byte("yadda"))
 
-    // for now, just return the token to the user. But we are going to want to wrap it in a jwt at some point with extra user information
-    //req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token")
-    //http.Post("https://accounts.spotify.com/api/token")
-   
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    c.SetCookie("test_cookie", tokenString, 3600, "/", "localhost", false, true)
+    
+    c.JSON(200, gin.H{
+        "message": "nice!",
+    })
 }
 
 
 
+type AppClaims struct {
+    spotifyID string 
+    accessToken string 
+    refreshToken string
+    accessTokenExpiresAt string
+    jwt.RegisteredClaims
+}
