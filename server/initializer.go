8d29b19e6e668c44afdaae4a588ec4dd5a11d7d2
Jack Gitter <jack.gitter@gmail.com>
2024-06-29 16:12:49 -0400

    jwt done

--- server/initializer.go
+++ server/initializer.go
@@ -5,6 +5,7 @@ import (
 	//"encoding/json"
 	//	"fmt"
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -25,23 +26,15 @@ import (
 
 func InitializeHttpServer() *gin.Engine {
     r := gin.Default()
-    r.GET("/test", helloWorld)
     r.GET("/auth", auth)
     r.GET("/accesstoken", accessToken)
     return r
 }
 
-func helloWorld(c *gin.Context) {
-    c.JSON(200, gin.H{
-        "message": "hello world!",
-    })
-}
 
 func auth(c *gin.Context) {
-
     endpoint := "https://accounts.spotify.com/authorize?response_type=code&client_id=83ada5f0555a4f57be4243c3788cc9f4&scope=user-read-private&redirect_uri=http://localhost:2000/accesstoken"
     c.Redirect(http.StatusMovedPermanently, endpoint) 
-
 }
 
 func accessToken(c *gin.Context) {
@@ -53,7 +46,14 @@ func accessToken(c *gin.Context) {
     req.Header.Set("Authorization", "Basic ODNhZGE1ZjA1NTVhNGY1N2JlNDI0M2MzNzg4Y2M5ZjQ6ODVkYjM4OTFiYzU0NDNmZGIxZDM2MDRjZjM5YTBhM2I=" )
 
     client := &http.Client{}
-    client.Do(req) 
+    resp, _ := client.Do(req) 
+
+    respJson := &AccessTokenResponnse{}
+
+    json.NewDecoder(resp.Body).Decode(respJson)
+
+    fmt.Println("the json is!!!")
+    fmt.Println(respJson)
 
     claims :=  jwt.MapClaims{
 		"sub": "test",
@@ -62,19 +62,15 @@ func accessToken(c *gin.Context) {
 		"exp": time.Now().Add(time.Hour).Unix(), 
 		"iat": time.Now().Unix(),
         "spotifyID": "testID",
-        "accessToken": "testToken",
-        "refreshToken": "testRefresh",
-        "accessTokenExpiresAt": "testDate",
+        "accessToken": respJson.Access_token,
+        "refreshToken": respJson.Refresh_token,
+        "accessTokenExpiresAt": respJson.Expires_in,
     }        
 
 
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-    tokenString, err := token.SignedString([]byte("yadda"))
-
-    if err != nil {
-        fmt.Println(err)
-    }
+    tokenString, _ := token.SignedString([]byte("yadda"))
 
     c.SetCookie("test_cookie", tokenString, 3600, "/", "localhost", false, true)
     
@@ -85,10 +81,12 @@ func accessToken(c *gin.Context) {
 
 
 
-type AppClaims struct {
-    spotifyID string 
-    accessToken string 
-    refreshToken string
-    accessTokenExpiresAt string
-    jwt.RegisteredClaims
+type AccessTokenResponnse struct {
+    Access_token string 
+    Token_type string 
+    Scope string 
+    Expires_in int 
+    Refresh_token string 
 }
+
+
