10f673d6c68115d042581efaff3b01b67c215490
Jack Gitter <jack.gitter@gmail.com>
2024-06-30 17:56:02 -0400

    going with graph database :(

--- db/dbConnection.go
+++ db/dbConnection.go
@@ -1,33 +1,8 @@
 package db
 
-import (
-	"database/sql"
-    _ "github.com/lib/pq"
-	"fmt"
-)
+import ()
 
-var DBConnection *sql.DB = nil
-
-func ConnectToDB() *sql.DB {
-
-    connectionString := "host=localhost port=5432 user=postgres password=04122001 dbname=tunes sslmode=disable"
-    db, err := sql.Open("postgres", connectionString)
-
-    DBConnection = db
-
-    if err != nil {
-        fmt.Println(err)
-    }
-
-    r := DBConnection.Ping()
-
-
-    if r != nil {
-        fmt.Println(r)
-        db.Close()
-    }
-
-    return db
-
+func ConnectToDB() {
+    // to-do
 }
 
