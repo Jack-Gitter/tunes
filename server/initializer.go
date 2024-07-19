43e0eea3ed68b4773bb5e8288b5d59f3431c6011
Jack Gitter <jack.gitter@gmail.com>
2024-07-18 22:04:24 -0400

    ok

--- server/initializer.go
+++ server/initializer.go
@@ -32,8 +32,8 @@ func InitializeHttpServer() *gin.Engine {
     r.GET("/currentUserPostPreviews/", auth.ValidateUserJWT, posts.GetAllPostsForCurrentUser)
     r.GET("/specificUserPostPreviews/:spotifyID", auth.ValidateUserJWT, posts.GetAllPostsForUserByID)
     r.POST("/posts", auth.ValidateUserJWT, posts.CreatePostForCurrentUser)
-    r.PATCH("/currentUserPosts/:songID", auth.ValidateUserJWT, posts.UpdateCurrentUserPost)
     r.POST("/posts/like/:spotifyID/:songID", auth.ValidateUserJWT, posts.LikePost)
+    r.PATCH("/currentUserPosts/:songID", auth.ValidateUserJWT, posts.UpdateCurrentUserPost)
     r.DELETE("/posts/:spotifyID/:songID", auth.ValidateUserJWT,  posts.DeletePostBySpotifyIDAndSongID)
     r.DELETE("/currentUserPosts/:songID", auth.ValidateUserJWT,  posts.DeletePostForCurrentUserBySongID)
 
