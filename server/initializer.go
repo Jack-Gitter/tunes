a84e62527a341d888d762aa1d994f9b0def8c0d5
Jack Gitter <jack.gitter@gmail.com>
2024-07-26 17:26:29 -0400

    working!

--- server/initializer.go
+++ server/initializer.go
@@ -11,33 +11,59 @@ import (
 func InitializeHttpServer() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/login", customerrors.ErrorHandlerMiddleware, auth.Login)
-	r.GET("/loginCallback", customerrors.ErrorHandlerMiddleware, auth.LoginCallback)
+    baseGroup := r.Group("/", customerrors.ErrorHandlerMiddleware)
+    {
+        loginGroup := baseGroup.Group("/login") 
+        {
+            loginGroup.GET("/", auth.Login)
+            loginGroup.GET("/callback", auth.LoginCallback)
+            loginGroup.GET("/jwt", auth.RefreshJWT)
+        }
 
-	r.POST("/refreshJWT", customerrors.ErrorHandlerMiddleware, auth.RefreshJWT)
+        authGroup := baseGroup.Group("", auth.ValidateUserJWT) 
+        {
 
-	r.GET("/users/:spotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.GetUserById)
-	r.GET("/users/current", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.GetCurrentUser)
-	r.GET("/users/current/followers", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.GetFollowers)
-	r.GET("/users/:spotifyID/followers", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.GetFollowersByID)
-	r.POST("/users/current/follow/:otherUserSpotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.FollowerUser)
-	r.POST("/users/current/unfollow/:otherUserSpotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.UnFollowUser)
-	r.PATCH("/users/current", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.UpdateCurrentUserProperties)
-	r.PATCH("/users/:spotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, auth.ValidateAdminUser, users.UpdateUserBySpotifyID)
-	r.DELETE("/users/current", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, users.DeleteCurrentUser)
-    r.DELETE("/users/:spotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, auth.ValidateAdminUser, users.DeleteUserBySpotifyID)
+            userGroup := authGroup.Group("/users")
+            {
+                userGroup.GET("/:spotifyID", users.GetUserById)
+                userGroup.GET("/current", users.GetCurrentUser)
+                userGroup.GET("/current/followers", users.GetFollowers)
+                userGroup.GET("/:spotifyID/followers", users.GetFollowersByID)
+                userGroup.POST("/current/follow/:otherUserSpotifyID", users.FollowerUser)
+                userGroup.POST("/current/unfollow/:otherUserSpotifyID", users.UnFollowUser)
+                userGroup.PATCH("/current", users.UpdateCurrentUserProperties)
+                userGroup.DELETE("/current", users.DeleteCurrentUser)
 
-	r.GET("/posts/:spotifyID/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.GetPostBySpotifyIDAndSongID)
-	r.GET("/posts/current/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.GetPostCurrentUserBySongID)
-	r.GET("/posts/previews/users/current", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.GetAllPostsForCurrentUser)
-	r.GET("/posts/previews/users/:spotifyID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.GetAllPostsForUserByID)
-	r.POST("/posts", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.CreatePostForCurrentUser)
-	r.POST("/posts/likes/:spotifyID/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.LikePost)
-	r.POST("/posts/dislikes/:spotifyID/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.DislikePost)
-	r.PATCH("/posts/current/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.UpdateCurrentUserPost)
-	r.DELETE("/posts/:spotifyID/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, auth.ValidateAdminUser, posts.DeletePostBySpotifyIDAndSongID)
-	r.DELETE("/posts/current/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.DeletePostForCurrentUserBySongID)
-	r.DELETE("/posts/votes/current/:posterSpotifyID/:songID", customerrors.ErrorHandlerMiddleware, auth.ValidateUserJWT, posts.RemovePostVote)
+                adminOnly := userGroup.Group("", auth.ValidateAdminUser)
+                {
+                    adminOnly.PATCH("/:spotifyID",  users.UpdateUserBySpotifyID)
+                    adminOnly.DELETE("/:spotifyID", users.DeleteUserBySpotifyID)
+                }
+
+            }
+
+            postGroup := authGroup.Group("/posts")
+            {
+
+                postGroup.GET("/:spotifyID/:songID", posts.GetPostBySpotifyIDAndSongID)
+                postGroup.GET("/current/:songID", posts.GetPostCurrentUserBySongID)
+                postGroup.GET("/previews/users/current", posts.GetAllPostsForCurrentUser)
+                postGroup.GET("/previews/users/:spotifyID", posts.GetAllPostsForUserByID)
+                postGroup.POST("/", posts.CreatePostForCurrentUser)
+                postGroup.POST("/likes/:spotifyID/:songID", posts.LikePost)
+                postGroup.POST("/dislikes/:spotifyID/:songID", posts.DislikePost)
+                postGroup.PATCH("/current/:songID", posts.UpdateCurrentUserPost)
+                postGroup.DELETE("/current/:songID", posts.DeletePostForCurrentUserBySongID)
+                postGroup.DELETE("/votes/current/:posterSpotifyID/:songID",  posts.RemovePostVote)
+
+                adminOnly := postGroup.Group("", auth.ValidateAdminUser)
+                {
+                    adminOnly.DELETE("/:spotifyID/:songID", posts.DeletePostBySpotifyIDAndSongID)
+                }
+
+            }
+        }
+    }
 
 	return r
 }
