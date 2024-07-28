89f94fde769a14184b8cc577b0c9921d1b8a7375
Jack Gitter <jack.gitter@gmail.com>
2024-07-28 12:12:13 -0400

    working on query builder

--- db/helpers/queryBuilder.go
+++ db/helpers/queryBuilder.go
@@ -1,13 +1,35 @@
 package helpers
 
-type QueryBuilder struct {
-
-    setcount int
-    wherecount int
-    query string
+import "fmt"
 
 
+func PatchQueryBuilder(table string, setParams map[string]interface{}, whereParams map[string]interface{}) string {
 
+    paramCount := 0
+    query := fmt.Sprintf(`UPDATE %s SET`, table)
+
+    for key, value := range setParams {
+        if value != nil {
+            query += fmt.Sprintf(` %s = $%d,`, key, paramCount)
+            paramCount+=1
+        } 
+    }
+
+    query = query[:len(query)-1]
+
+    if len(whereParams) == 0 {
+        return query
+    }
+
+    query += `WHERE`
+
+    for key, value := range whereParams {
+        if value != nil {
+            query += fmt.Sprintf(` %s = $%d,`, key, paramCount)
+            paramCount+=1
+        }
+    }
+
+    return query[:len(query)-1]
 
 }
-
